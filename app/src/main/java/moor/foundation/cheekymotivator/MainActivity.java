5967c67c8c40a1ae9f6474c643771b5282bd7e93
Mikhailmoor74 <mikhail.moor@bk.ru>
2020-12-21 19:53:24 +0500

    first add

--- app/src/main/java/moor/foundation/cheekymotivator/MainActivity.java
+++ app/src/main/java/moor/foundation/cheekymotivator/MainActivity.java
@@ -1,7 +1,9 @@
 package moor.foundation.cheekymotivator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
     }
 }
